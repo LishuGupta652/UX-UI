66344c449432524f6eace831ea19a23f312a2db2
lishu <lishugupta652@gmail.com>
2021-12-21 15:35:05 +0530

    Heaader

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 const Nav = styled.nav`
-  background: #ffcccc;
+  background: #474787;
   padding: 20px;
+  color: white;
 `;
 const Flex = styled.div`
   display: flex;
   justify-content: space-around;
-
+  align-items: center;
   ul {
-    display: inline-block;
+    display: flex;
+    li {
+      margin-right: 20px;
+      font-size: 1.2rem;
+    }
   }
 `;
 const Header = () => {
