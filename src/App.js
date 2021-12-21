f00948b1575fc514ee1f55885b3c600f79309097
lishu <lishugupta652@gmail.com>
2021-12-21 15:01:02 +0530

    Router Setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import "./App.css";
 
 import { ThemeProvider } from "styled-components";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import ReactFibar from "./pages/ReactFibar";
+
 const lightTheme = {
   black: "#121212",
   white: "#fff",
 };
 
 function App() {
-  return <ThemeProvider theme={lightTheme}></ThemeProvider>;
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}>
+            <Route path="/fiber" element={<ReactFibar />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/ReactFibar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ReactFibar = () => {
+  return <div>react fiber</div>;
+};
+
+export default ReactFibar;
