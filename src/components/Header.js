d41d2aaf71190105923336fb88568f7cdf30948c
lishu <lishugupta652@gmail.com>
2021-12-21 15:32:59 +0530

    Heaader

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 
-const Nav = styled.nav``;
-const Flex = styled.div``;
+const Nav = styled.nav`
+  background: #ffcccc;
+  padding: 20px;
+`;
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  ul {
+    display: inline-block;
+  }
+`;
 const Header = () => {
   return (
     <Nav>
