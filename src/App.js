affcd460b9433e517d5992a3c62439cfc616f269
lishu <lishugupta652@gmail.com>
2021-12-21 14:55:22 +0530

    Theme context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
+import { ThemeProvider } from "styled-components";
+
+const lightTheme = {
+  black: "#121212",
+  white: "#fff",
+};
+
 function App() {
-  return <div className="App">react 3d</div>;
+  return <ThemeProvider theme={lightTheme}></ThemeProvider>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { ThemeContext } from "./Context/ThemeContext";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeContext>
+      <App />
+    </ThemeContext>
   </React.StrictMode>,
   document.getElementById("root")
 );
