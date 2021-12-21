be76e4981ba55208ff16316edc657ff5b1d1b2f5
lishu <lishugupta652@gmail.com>
2021-12-21 15:06:43 +0530

    Not Found Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>Not Found Please visit home page or try again later.</div>;
+  return (
+    <div>
+      <h1>404 Route not found</h1>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
 };
 
 export default NotFound;
