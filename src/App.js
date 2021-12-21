413edb960f116fac8c69a9639a0d6949eff66c14
lishu <lishugupta652@gmail.com>
2021-12-21 20:06:03 +0530

    Heaader

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { ThemeProvider } from "styled-components";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
@@ -8,7 +8,6 @@ import ReactFibar from "./pages/ReactFibar";
 
 import { lightTheme, GlobalStyle } from "./globals/GlobalStyle";
 import NotFound from "./pages/NotFound";
-import Header from "./components/Header";
 
 function App() {
   return (
