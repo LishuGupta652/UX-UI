706ace892155356ef474ec06dd5638a2bc228e4b
lishu <lishugupta652@gmail.com>
2021-12-22 12:15:49 +0530

    PROJECT-01 - REACT-MARKDON

--- src/pages/markdown/MarkdownIndex.js
+++ src/pages/markdown/MarkdownIndex.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import reactMarkdown from "react-markdown";
 import styled from "styled-components";
 
 const MarkdownStyle = styled.div`
@@ -10,6 +11,12 @@ const MarkdownStyle = styled.div`
     font-size: 2rem;
     outline: none;
   }
+  .markdown {
+    width: 50%;
+    height: 100vh;
+    padding: 20px;
+    outline: none;
+  }
 `;
 
 const MarkdownIndex = () => {
@@ -17,10 +24,12 @@ const MarkdownIndex = () => {
   return (
     <MarkdownStyle>
       <textarea
+        autoFocus
         value={input}
         onChange={(e) => setInput(e.target.value)}
         className="textarea"
       ></textarea>
+      <reactMarkdown source={input} className="markdown" />
     </MarkdownStyle>
   );
 };
